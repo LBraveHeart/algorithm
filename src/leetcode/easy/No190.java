7b230d8ded803af5205c97e59fe70b99bfca2c50
liuyy <liuyaoyao@oxcoder.cn>
2020-07-07 14:20:54 +0800

    解题

--- src/leetcode/easy/No190.java
+++ src/leetcode/easy/No190.java
@@ -5,4 +5,19 @@ package leetcode.easy;
  * @date: 2020/7/7 14:12
  */
 public class No190 {
+    public static void main(String[] args) {
+        No190 no190 = new No190();
+        no190.reverseBits(43261596);
+    }
+    public int reverseBits(int n) {
+
+        String string = Integer.toBinaryString(n);
+
+        StringBuilder sb = new StringBuilder();
+        char[] chars = string.toCharArray();
+        for (int i = chars.length - 1; i >= 0; i--) {
+            sb.append(chars[i]);
+        }
+        return Integer.parseInt(sb.toString());
+    }
 }
